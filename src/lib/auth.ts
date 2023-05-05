fbc48c3698f0f4a898021ee6a038e5a571dcda2a
Vaibhav Deep <vaibhavdeep46@gmail.com>
2023-05-05 21:04:31 +0530

    [add]: nextauth config file

---
+++ src/lib/auth.ts
@@ -0,0 +1,14 @@
+import { NextAuthOptions } from "next-auth";
+import { UpstashRedisAdapter } from "@next-auth/upstash-redis-adapter";
+import { db } from "./db";
+
+export const authOptions: NextAuthOptions = {
+  adapter: UpstashRedisAdapter(db),
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/login",
+  },
+  providers: [],
+};
