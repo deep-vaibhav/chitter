2e6c27ab9211927f6b8f2ee10050ec10a9741899
Vaibhav Deep <vaibhavdeep46@gmail.com>
2023-05-05 21:47:41 +0530

    [add:ui]: login page UI

---
+++ src/app/login/page.tsx
@@ -0,0 +1,42 @@
+"use client"; // make this explicity a client component
+
+import Button from "@/components/ui/Button";
+import { SVGIcons } from "@/components/ui/Icons";
+import { FC, useState } from "react";
+
+interface LoginProps {}
+
+const Login: FC<LoginProps> = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const loginWithGoogle = async () => {};
+
+  return (
+    <>
+      <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div className="w-full flex flex-col items-center max-w-md space-y-8">
+          <div className="flex flex-col items-center gap-8">
+            <p className="font-extrabold tracking-wide text-cyan-700">
+              chitter
+            </p>
+            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight">
+              Sign in to your account
+            </h2>
+          </div>
+
+          <Button
+            isLoading={isLoading}
+            type="button"
+            className="max-w-sm max-auto"
+            onClick={loginWithGoogle}
+          >
+            <SVGIcons.GoogleIcon size={24} />
+            <span className="ml-2">Continue with Google</span>
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
