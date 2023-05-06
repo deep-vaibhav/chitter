fa15e1c6cdf792ee94909e73ebb66755428f6df7
Vaibhav Deep <deep.vaibhav79@gmail.com>
2023-05-07 00:13:36 +0530

    [add]: signout and profile info component

---
+++ src/components/SignOutButton.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { ButtonHTMLAttributes, FC, useState } from "react";
+import Button from "./ui/Button";
+import { signOut } from "next-auth/react";
+import { Loader2, LogOut } from "lucide-react";
+
+interface SignOutButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
+
+const SignOutButton: FC<SignOutButtonProps> = ({ ...props }) => {
+  const [isSigningOut, setIsSigningOut] = useState<boolean>(false);
+
+  return (
+    <Button
+      variant="ghost"
+      {...props}
+      onClick={async () => {
+        setIsSigningOut(true);
+        try {
+          await signOut();
+        } catch (error) {
+          // show error toast
+        } finally {
+          setIsSigningOut(false);
+        }
+      }}
+    >
+      {isSigningOut ? (
+        <Loader2 className="animate-spin h-4 w-4" />
+      ) : (
+        <LogOut className="w-4 h-4" />
+      )}
+    </Button>
+  );
+};
+
+export default SignOutButton;
